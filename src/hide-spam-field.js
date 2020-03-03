2ba78e734944f089ad47ea988f70d40d0f00080e
garrystebbins <garry.stebbins77@gmail.com>
2020-03-03 11:51:54 -0600

    feat: close the form and redirect (#1)

--- src/hide-spam-field.js
+++
@@ -1,8 +0,0 @@
-// hide the honeypot field
-// using JS instead of CSS because I *think* spam bots respect CSS now
-// also I could be making that up
-// I feel like I read that somewhere though
-// and, like, if it’s on the internet it’s true, right?
-const spam = document.querySelector('label.hidden');
-
-spam.style.display = 'none';
